d15073d970915e7a55acac666ad2005243a12269
Tarun_Verma <59416189+Tarunverma504@users.noreply.github.com>
2020-11-06 01:00:42 +0530

    Update js.js

--- js.js
+++ js.js
@@ -5,10 +5,8 @@ function fun()
 
 	setTimeout(function(){
         window.location.href = 'firstpage.html';
-    }, 6000);
+    }, 7000);
 }
-
-
 function fun2()
 {
 	player1=document.getElementById("play1").value.trim();
@@ -21,38 +19,47 @@ function fun2()
 	}
 	else
 	{
-		console.log(player1);
-		console.log(player2);
-		console.log('new page');
+		//console.log(player1);
+		//console.log(player2);
+		//console.log('new page');
 		window.location.href = 'game.html';
 	}
-
 }
-var count=0;
-var ccount=0;
+var flag=0;
 var Tarun=1;
 var player1=localStorage.getItem("player1");
 var player2=localStorage.getItem("player2");
 function om()
 {
 	
-	document.getElementById('first').value=player1+":"+count;
+	document.getElementById('first').value=player1;
 	document.getElementById('second').value=player1+" turn";
-	document.getElementById('third').value=player2+":"+ccount;
+	document.getElementById('third').value=player2;
+}	
+var arr=new Array(3);
+var i=0,str1,str2,k=0;
+for(i=0;i<3;i++)
+{
+	arr[i]=new Array(3);
+}
+for(i=0;i<3;i++)
+{
+	for(var j=0;j<3;j++)
+	{
+		arr[i][j]=k++;
+	}
 }
 function func3(a)
 {
+
 	console.log(document.getElementById('dog').children);
-
-
-
-
-
-
-
-
-
-
+	str1=a.slice(1,2);
+	str2=a.slice(2);
+	//console.log("~~"+str1);
+	//console.log("~~~"+str2);
+	//console.log("~"+a);
+	//arr[str1][str2]=1;
+	//console.log(arr[str1][str2]);
 
 
 	if(Tarun%2==0)
@@ -62,6 +69,7 @@ function func3(a)
 		document.getElementById(a).style.color='white';
 		document.getElementById(a).style.fontSize="25px";
 		document.getElementById(a).disabled=true;
+		arr[str1][str2]='0';
 	}
 	else{
 		document.getElementById('second').value=player2 +" turn";
@@ -69,7 +77,104 @@ function func3(a)
 		document.getElementById(a).style.color='white';
 		document.getElementById(a).style.fontSize="25px";
 		document.getElementById(a).disabled=true;
+		arr[str1][str2]='X';
+	}
+	if(arr[0][0]==arr[0][1] && arr[0][1]==arr[0][2])//1 horizontal
+	{
+		if(arr[0][0]=='X'){ document.getElementById('second').value=player1+" Wins"; }
+		else{document.getElementById('second').value=player2+" Wins";}
+         document.getElementById('second').style.color=" red";
+         document.getElementsByClassName('btn').disabled=true;
+         document.getElementById("_00").style.backgroundColor="orange";
+         document.getElementById("_01").style.backgroundColor="orange";
+         document.getElementById("_02").style.backgroundColor="orange";
+         flag=1;
+		console.log("~"+arr[0][1]);
+	}
+	else if(arr[1][0]==arr[1][1] && arr[1][1]==arr[1][2])//2 horizontal
+	{
+		if(arr[1][0]=='X'){ document.getElementById('second').value=player1+" Wins";}
+		else{document.getElementById('second').value=player2+" Wins";}
+         document.getElementById('second').style.color=" red";
+          document.getElementById("_10").style.backgroundColor="orange";
+         document.getElementById("_11").style.backgroundColor="orange";
+         document.getElementById("_12").style.backgroundColor="orange";
+		console.log("~~"+arr[1][0]);
+		flag=1;
+	}
+	else if(arr[2][0]==arr[2][1] && arr[2][1]==arr[2][2])// 3 horizontal
+	{
+		if(arr[2][0]=='X'){ document.getElementById('second').value=player1+" Wins";}
+		else{document.getElementById('second').value=player2+" Wins";}
+         document.getElementById('second').style.color=" red";
+          document.getElementById("_20").style.backgroundColor="orange";
+         document.getElementById("_21").style.backgroundColor="orange";
+         document.getElementById("_22").style.backgroundColor="orange";
+         flag=1;
+		console.log("~~~"+arr[2][0]);
+	}
+	else if(arr[0][0]==arr[1][0] && arr[1][0]==arr[2][0])//1 vertical
+	{
+		if(arr[0][0]=='X'){ document.getElementById('second').value=player1+" Wins";}
+		else{document.getElementById('second').value=player2+" Wins";}
+         document.getElementById('second').style.color=" red";
+          document.getElementById("_00").style.backgroundColor="orange";
+         document.getElementById("_10").style.backgroundColor="orange";
+         document.getElementById("_20").style.backgroundColor="orange";
+         flag=1;
+		console.log("~~~~"+arr[0][0]);
+	}
+	else if(arr[0][1]==arr[1][1] && arr[1][1]==arr[2][1])//2 vertical
+	{
+		if(arr[0][1]=='X'){ document.getElementById('second').value=player1+" Wins"; }
+		else{document.getElementById('second').value=player2+" Wins";}
+         document.getElementById('second').style.color=" red";
+          document.getElementById("_01").style.backgroundColor="orange";
+         document.getElementById("_11").style.backgroundColor="orange";
+         document.getElementById("_21").style.backgroundColor="orange";
+         flag=1;
+		console.log("~~~~~~"+arr[0][1]);
+	}
+	else if(arr[0][2]==arr[1][2] && arr[1][2]==arr[2][2])//3 vertical
+	{
+		if(arr[0][2]=='X'){ document.getElementById('second').value=player1+" Wins";}
+		else{document.getElementById('second').value=player2+" Wins";}
+         document.getElementById('second').style.color=" red";
+          document.getElementById("_02").style.backgroundColor="orange";
+         document.getElementById("_12").style.backgroundColor="orange";
+         document.getElementById("_22").style.backgroundColor="orange";
+         flag=1;
+		console.log("~~~~~~~"+arr[0][2]);
+	}
+	else if(arr[0][0]==arr[1][1] && arr[1][1]==arr[2][2])//1 diagonal
+	{
+		if(arr[0][0]=='X'){ document.getElementById('second').value=player1+" Wins"; }
+		else{document.getElementById('second').value=player2+" Wins";}
+         document.getElementById('second').style.color=" red";
+          document.getElementById("_00").style.backgroundColor="orange";
+         document.getElementById("_11").style.backgroundColor="orange";
+         document.getElementById("_22").style.backgroundColor="orange";
+         flag=1;
+		console.log("~~~~~~~~"+arr[0][0]);
+	}
+	else if(arr[0][2]==arr[1][1] && arr[1][1]==arr[2][0])//1 diagonal
+	{
+		if(arr[0][2]=='X'){ document.getElementById('second').value=player1+" Wins";}
+		else{document.getElementById('second').value=player2+" Wins";}
+         document.getElementById('second').style.color=" red";
+          document.getElementById("_02").style.backgroundColor="orange";
+         document.getElementById("_11").style.backgroundColor="orange";
+         document.getElementById("_20").style.backgroundColor="orange";
+         flag=1;
+		console.log("~~~~~~~~~"+arr[0][2]);
+	}
+	if(Tarun==9)
+	{ if(flag==0){
+		document.getElementById('second').value="Tie";
+		document.getElementById('second').style.color=" green";}
+
 	}
 	Tarun++;
-	console.log(a);
+	console.log(arr);
+	//console.log(a);
 }
